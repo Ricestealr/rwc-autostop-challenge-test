5774d605ae472f6cbea5e8a53fc60d53fa538420
jamesmcildowietfl <undefined>
2022-04-22 13:30:32 +0000

    Added desired datalogger output

--- main.cpp
+++ main.cpp
@@ -29,7 +29,8 @@ bool emergencyStopActive = false;
 //Display Function for data logger
 void DisplaySerial(){ 
     std::stringstream displayline;
-    displayline << "Blackbox# " << " Motor Accelerator: " << motorAccelerator << " " << "Emergency Stop Status: " << emergencyStopActive << " "  << "Drive Mode: " << driveMode  << " " ; // + "Current Speed: " +(int)dashboard.currentSpeed;
+    displayline << "Blackbox# " << " Motor Accelerator: " << motorAccelerator << " Brake 3/2: " << brakeValve32 <<  " Brake 2/2: " << brakeValve22 <<  " Speed: " << dashboard.currentSpeed  " Drive Mode: " << driveMode;  
+    //displayline << "Blackbox# " << " Motor Accelerator: " << motorAccelerator << " " << "Emergency Stop Status: " << emergencyStopActive << " "  << "Drive Mode: " << driveMode  << " " ; // + "Current Speed: " +(int)dashboard.currentSpeed;
     string disp=displayline.str();
     pc.printf("%s \n",disp);
     }
